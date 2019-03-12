dda9db181b32d9e4368176d66b0974d84d457f47
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-12 07:02:10 -0400

    added movement

--- GameEngineUI/GameEngineUI.cpp
+++ GameEngineUI/GameEngineUI.cpp
@@ -21,6 +21,14 @@ private:
 	int nLevelWidth;
 	int nLevelHeight;
 
+	//player position coordinates.
+	float fPlayerPosX = 0.0f;
+	float fPlayerPosY = 0.0f;
+
+	//player position coordinates.
+	float fPlayerVelX = 0.0f;
+	float fPlayerVelY = 0.0f;
+
 	//2D camera.
 	float fCameraPosX = 0.0f;
 	float fCameraPosY = 0.0f;
@@ -29,37 +37,79 @@ protected:
 	virtual bool OnUserCreate() {
 		
 		//initialize game tile dimension.
-		nLevelHeight = 64;
+		nLevelWidth = 64;
 		nLevelHeight = 16;
 
 		//symbolic level map.
-		sLevel += L".............................................................";
-		sLevel += L".............................................................";
-		sLevel += L".............................................................";
-		sLevel += L".....................###.....................................";
-		sLevel += L"...................#.............#.#.........................";
-		sLevel += L".................#...........................................";
-		sLevel += L"############################.################################";
-		sLevel += L".............................................................";
-		sLevel += L"............................#................................";
-		sLevel += L"...........................#.................................";
-		sLevel += L"..........................#..................................";
-		sLevel += L".......................#.....................................";
-		sLevel += L"........................####.................................";
-		sLevel += L".............................................................";
-		sLevel += L".............................................................";
-		sLevel += L".............................................................";
+		sLevel += L"...............................................................";
+		sLevel += L"...............................................................";
+		sLevel += L"...............................................................";
+		sLevel += L".....................###.......................................";
+		sLevel += L"...................#.............#.#...........................";
+		sLevel += L".................#.............................................";
+		sLevel += L"############################.################################..";
+		sLevel += L"...............................................................";
+		sLevel += L"............................#..................................";
+		sLevel += L"...........................#...................................";
+		sLevel += L"..........................#....................................";
+		sLevel += L".......................#.......................................";
+		sLevel += L"........................####...................................";
+		sLevel += L"...............................................................";
+		sLevel += L"...............................................................";
+		sLevel += L"...............................................................";
 		
 
 		return true;
 	}
 
 	virtual bool OnUserUpdate(float fElapsedtime) {
+		//Utility lambdas.
+		auto GetTile = [&](int x, int y) {
+			if (x >= 0 && x < nLevelWidth && y >= 0 && y < nLevelHeight) {
+				return sLevel[y * nLevelWidth + x];
+			}
+			else {
+				return L' ';
+			}
+		};
+
+		auto SetTile = [&](int x, int y, wchar_t c) {
+			if (x >= 0 && x < nLevelWidth && y >= 0 && y < nLevelHeight) {
+				return sLevel[y * nLevelWidth + x] = c;
+			}
+		};
+
+		fPlayerVelX = 0.0f;
+		fPlayerVelY = 0.0f;
+
+		//Handle input.
+		if (IsFocused()) {
+			if (GetKey(VK_UP).bHeld) {
+				fPlayerVelY = -6.0f;
+			}
+
+			if (GetKey(VK_LEFT).bHeld) {
+				fPlayerVelX = -6.0f;
+			}
+
+			if (GetKey(VK_DOWN).bHeld) {
+				fPlayerVelY = 6.0f;
+			}
+
+			if (GetKey(VK_RIGHT).bHeld) {
+				fPlayerVelX = 6.0f;
+			}
+		}
+
+		fPlayerPosX = fPlayerPosX + fPlayerVelX * fElapsedtime;
+		fPlayerPosY = fPlayerPosY + fPlayerVelY * fElapsedtime; 
 		
+		fCameraPosX = fPlayerPosX;
+		fCameraPosY = fPlayerPosY;
+
 		//Draw level.
 		int nTileWidth = 16;
-		int nTileHeight = 64;
-
+		int nTileHeight = 16;
 		int nVisibleTilesX = ScreenWidth() / nTileWidth;
 		int nVisibleTilesY = ScreenHeight() / nTileHeight;
 
@@ -71,6 +121,34 @@ protected:
 		if (fOffsetX < 0) fOffsetX = 0;
 		if (fOffsetY < 0) fOffsetY = 0;
 
+		if (fOffsetX > nLevelWidth - nVisibleTilesX) fOffsetX = nLevelWidth - nVisibleTilesX;
+		if (fOffsetY > nLevelHeight - nVisibleTilesY) fOffsetY = nLevelHeight - nVisibleTilesY;
+
+		// Draw tiles map.
+		for (int x = -1; x < nVisibleTilesX +1; x++)
+		{
+			for (int y = -1; y < nVisibleTilesY +1; y++)
+			{
+				wchar_t sTileID = GetTile(x +fOffsetX, y + fOffsetY);
+
+				//decide what to draw on the tile.
+				switch (sTileID)
+				{
+				case L'.':
+					Fill(x * nTileWidth, y * nTileHeight, (x + 1) * nTileWidth, (y + 1) * nTileHeight, PIXEL_SOLID, FG_CYAN);
+					break;
+				case L'#':
+					Fill(x * nTileWidth, y * nTileHeight, (x + 1) * nTileWidth, (y + 1) * nTileHeight, PIXEL_SOLID, FG_RED);
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
+		//Draw Player.
+		Fill((fPlayerPosX - fOffsetX) * nTileWidth, (fPlayerPosY - fOffsetY) * nTileHeight, (fPlayerPosX - fOffsetX + 1.0f) * nTileWidth, (fPlayerPosY - fOffsetY + 1.0f) * nTileHeight, PIXEL_SOLID, FG_RED);
+
 		return true;
 	}
 };
@@ -79,7 +157,7 @@ protected:
 int main()
 {
 	OneLoneCoder_Platform game;
-	if (game.ConstructConsole(200, 120, 8, 8))
+	if (game.ConstructConsole(160, 120, 4, 4))
 		game.Start();
 
 	return 0;
